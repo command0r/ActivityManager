af9f440f08bc08f6d28934d9b0284bf126a35eb2
Alex Kaziuka <akazyuka@gmail.com>
2023-01-17 22:30:57 -0500

    Introducing Activity interface and the code optimization

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,14 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
 import {Header, List, ListItem} from "semantic-ui-react";
+import {Activity} from "../models/activity";
 
 function App() {
   // Use a 'useState' hook to store activities when the results get back from the API
   // Give 'useState' an empty array to get around the 'activities' variable render as 'undefined'
-  const [activities, setActivities] = useState([]);
+  const [activities, setActivities] = useState<Activity[]>([]);
   
   useEffect(() => {
-     axios.get('http://localhost:5000/api/Activities')
+     axios.get<Activity[]>('http://localhost:5000/api/Activities')
          .then(response => {
            // console.log(response);
            setActivities(response.data);
@@ -21,7 +22,7 @@ function App() {
       <Header as='h2' icon='users' content='Activities' />
         
         <List>
-            {activities.map((activity: any) => (
+            {activities.map(activity => (
                 <ListItem key={activity.id}>
                     {activity.title}
                 </ListItem>
