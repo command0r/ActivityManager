9de683bf9f18b877ee720e08643c46ec813798fe
Alex Kaziuka <akazyuka@gmail.com>
2023-01-24 22:52:04 -0500

    Refactoring state mgmt logic with MobX

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,10 +1,7 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import {Container} from "semantic-ui-react";
-import {Activity} from "../models/activity";
 import NavBar from "./NavBar";
 import ActivitiesDashboard from "../../features/activities/dashboard/ActivitiesDashboard";
-import {v4 as uuid} from 'uuid';
-import agent from "../api/agent";
 import LoadingComponent from "./LoadingComponent";
 import {useStore} from "../stores/store";
 import { observer } from 'mobx-react-lite';
@@ -12,88 +9,19 @@ import { observer } from 'mobx-react-lite';
 function App() {
   // Use store (destructuring object to access ActivityStore)
   const {activityStore} = useStore();
-    
-  // Use a 'useState' hook to store activities when the results get back from the API
-  // Give 'useState' an empty array to get around the 'activities' variable render as 'undefined'
-  const [activities, setActivities] = useState<Activity[]>([]);
-  
-  // The initial state of Activity is 'undefined' (logic for passing mouse click and edit)
-  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined);
-  const [editMode, setEditMode] = useState(false);
-  const [submitting, setSubmitting] = useState(false);
   
   useEffect(() => {
     activityStore.loadActivities();
     // Set dependencies so that the call only happens once
   }, [activityStore])
-    
-  // Logic to pass down the button click on a specific activity to a downstream component
-  function handleSelectActivity(id: String) {
-      setSelectedActivity(activities.find(x => x.id === id));
-  }
-  
-  function handleCancelSelectActivity() {
-      setSelectedActivity(undefined);
-  }
-  
-  // Edit logic
-  function handleFormOpen(id?: string) {
-      id ? handleSelectActivity(id) : handleCancelSelectActivity();
-      setEditMode(true);
-  }
-  
-  function handleFormClose() {
-      setEditMode(false);
-  }
-  
-  function handleCreateOrEditActivity(activity: Activity) {
-      // Use 'filter' to remove Activities that are updated and replaced them 
-      // with the parameters passed to this func
-      setSubmitting(true);
-      if(activity.id) {
-          agent.Activities.update(activity).then(() => {
-              setActivities([...activities.filter(x => x.id !== activity.id), activity])
-              setSelectedActivity(activity);
-              setEditMode(false);
-              setSubmitting(false);
-          })
-      } else {
-          activity.id = uuid();
-          agent.Activities.create(activity).then(() => {
-              setActivities([...activities, activity])
-              setSelectedActivity(activity);
-              setEditMode(false);
-              setSubmitting(false);
-          })
-      }
-  }
-  
-  function handleDeleteActivity(id: string) {
-      setSubmitting(true);
-      agent.Activities.delete(id).then(() => {
-          setActivities([...activities.filter(x => x.id !== id)])
-          setSubmitting(false);
-      })
-  }
   
   if(activityStore.loadingInitial) return <LoadingComponent content='Loading app' />
       
   return (
     <div>
-      <NavBar openForm={handleFormOpen} />
+      <NavBar />
         <Container style={{marginTop: '7em'}}>
-            <ActivitiesDashboard 
-                activities={activityStore.activities}
-                selectedActivity={selectedActivity}
-                selectActivity={handleSelectActivity}
-                cancelSelectActivity={handleCancelSelectActivity}
-                editMode={editMode}
-                openForm={handleFormOpen}
-                closeForm={handleFormClose}
-                createOrEdit={handleCreateOrEditActivity}
-                deleteActivity={handleDeleteActivity}
-                submitting={submitting}
-            />
+            <ActivitiesDashboard />
         </Container>
     </div>
   );
--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -1,29 +1,36 @@
 import {makeAutoObservable, runInAction} from "mobx";
 import {Activity} from "../models/activity";
 import agent from "../api/agent";
+import {v4 as uuid} from 'uuid';
 
 export default class ActivityStore {
-    activities: Activity[] = [];
-    selectedActivity: Activity | null = null;
+    activityRegistry = new Map<string, Activity>();
+    
+    selectedActivity: Activity | undefined = undefined;
     editMode = false;
     loading = false;
-    loadingInitial = false;
-        
+    loadingInitial = true;
+
     constructor() {
         makeAutoObservable(this)
     }
     
+    // Computed property that work out the Activity dates in order
+    get activitiesByDate() {
+        return Array.from(this.activityRegistry.values()).sort((a, b) => 
+            Date.parse(a.date) - Date.parse(b.date));
+    }
+
     // 'Arrow' func, so that no need to worry about binding to a class
     // All async code is inside the try/catch block
     loadActivities = async () => {
-        this.loadingInitial = true;
         try {
             const activities = await agent.Activities.list();
             activities.forEach(activity => {
                 // Update date property before we 'set' activity
                 activity.date = activity.date.split('T')[0];
                 // Mutate the state in MobX (populate an array with received activities)
-                this.activities.push(activity);
+                this.activityRegistry.set(activity.id, activity);
             })
             this.setLoadingInitial(false);
         } catch (error) {
@@ -31,8 +38,88 @@ export default class ActivityStore {
             this.setLoadingInitial(false);
         }
     }
-    
+
     setLoadingInitial = (state: boolean) => {
         this.loadingInitial = state;
+    }
+
+    // Logic to pass down the button click on a specific activity to a downstream component
+    selectActivity = (id: string) => {
+        this.selectedActivity = this.activityRegistry.get(id);
+    }
+
+    cancelSelectedActivity = () => {
+        this.selectedActivity = undefined;
+    }
+
+    // Edit logic
+    openForm = (id?: string) => {
+        id ? this.selectActivity(id) : this.cancelSelectedActivity();
+        this.editMode = true;
+    }
+
+    closeForm = () => {
+        this.editMode = false;
+    }
+
+    // Create Activity method
+    createActivity = async (activity: Activity) => {
+        this.loading = true;
+        activity.id = uuid();
+        try {
+            await agent.Activities.create(activity);
+            // If update operation is successful the store needs to be updated
+            runInAction(() => {
+                this.activityRegistry.set(activity.id, activity);
+                this.selectedActivity = activity;
+                this.editMode = false;
+                this.loading = false;
+            })
+        } catch (error) {
+            console.log(error);
+            runInAction(() => {
+                this.loading = false;
+            })
+        }
+    }
+
+    // Update Activity method
+    updateActivity = async (activity: Activity) => {
+        this.loading = true;
+        try {
+            await agent.Activities.update(activity);
+            runInAction(() => {
+                // this.activities.filter(a => a.id !== activity.id);
+                // this.activities.push(activity); or:
+                this.activityRegistry.set(activity.id, activity);
+                this.selectedActivity = activity;
+                this.editMode = false;
+                this.loading = false;
+            })
+        } catch (error) {
+            console.log(error);
+            runInAction(() => {
+                this.loading = false;
+            })
+        }
+    }
+    
+    // Delete Activity
+    deleteActivity = async (id: string) => {
+        this.loading = true;
+        try {
+            await agent.Activities.delete(id);
+            runInAction(() => {
+                this.activityRegistry.delete(id);
+                
+                if (this.selectedActivity?.id === id) this.cancelSelectedActivity();
+                this.loading = false;
+            })
+        } catch (error) {
+            console.log(error);
+            runInAction(() => {
+                this.loading = false;
+            })
+        }
     }
 }
