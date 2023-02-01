7ce679d43ee399ad73033c35641672d1ba1fa2b7
Alex Kaziuka <akazyuka@gmail.com>
2023-01-31 21:03:45 -0500

    Add activities filters and home page styling

--- client-app/src/features/activities/dashboard/ActivityListItem.tsx
+++ client-app/src/features/activities/dashboard/ActivityListItem.tsx
@@ -1,25 +1,13 @@
-import React, {SyntheticEvent, useState} from "react";
-import {Button, Icon, Item, ItemGroup, Label, Segment} from "semantic-ui-react";
+import React from "react";
+import {Button, Icon, Item, Segment} from "semantic-ui-react";
 import {Link} from "react-router-dom";
 import {Activity} from "../../../app/models/activity";
-import {useStore} from "../../../app/stores/store";
 
 interface Props {
     activity: Activity
 }
 
 export default function ActivityListItem({activity}: Props) {
-    const {activityStore} = useStore();
-    const {deleteActivity, activitiesByDate, loading} = activityStore;
-
-    // Logic to check if the target matched the name of the button (so the loader only appears on a specific btn)
-    const [target, setTarget] = useState('');
-
-    function handleActivityDelete(e: SyntheticEvent<HTMLButtonElement>, id: string) {
-        setTarget(e.currentTarget.name);
-        deleteActivity(id);
-    }
-    
     return (
         <Segment.Group>
             <Segment>
